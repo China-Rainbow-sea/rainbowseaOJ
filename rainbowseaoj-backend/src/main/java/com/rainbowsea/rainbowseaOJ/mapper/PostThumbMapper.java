641cb15a5b3498d0fc96d4b21cccf867558620dc
RainbowSea <2969377330@qq.com>
2025-07-20 15:45:51 +0800

    后端项目初始化在线OJ刷题：实现用户登录，使用项目模板改造

---
+++ rainbowseaoj-backend/src/main/java/com/rainbowsea/rainbowseaOJ/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.rainbowsea.rainbowseaOJ.mapper;
+
+import com.rainbowsea.rainbowseaOJ.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author  
+ * @from   
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ rainbowseaoj-backend/src/main/java/com/rainbowsea/rainbowseaOJ/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.rainbowsea.rainbowseaOJ.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rainbowsea.rainbowseaOJ.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author  
+ * @from   
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
